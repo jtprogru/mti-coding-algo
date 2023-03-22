f1d003e17e5d22ac9c0629c72581cd311bbae018
Michael Savin <jtprogru@gmail.com>
2023-03-22 21:07:16 +0300

    upd: Add lab01 task 2.1

---
+++ lab01.2.1/src/main.cpp
@@ -0,0 +1,33 @@
+/* ЛАБОРАТОРНАЯ РАБОТА 1. Ввод и вывод
+Задание 2.
+Напишите программу, которая запрашивала бы у пользователя:
+    - его имя (например, "What is your name?")
+    - возраст ("How old are you?")
+    - место жительства ("Where are you live?")
+После этого выводила бы три строки:
+    "This is имя"
+    "It is возраст"
+    "(S)he live in место_жительства"
+Вместо имя, возраст, место_жительства должны быть данные, введенные
+пользователем.
+
+Copyright [2023] Michael Savin
+*/
+
+#include <iostream>
+
+int main() {
+  std::string name;
+  std::uint16_t age;
+  std::string place;
+  std::cout << "What is your name?\n";
+  std::cin >> name;
+  std::cout << "How old are you?\n";
+  std::cin >> age;
+  std::cout << "Where are you live?\n";
+  std::cin >> place;
+  std::cout << "This is " << name << std::endl;
+  std::cout << "It is " << age << std::endl;
+  std::cout << "(S)he live in " << place << std::endl;
+  return 0;
+}
