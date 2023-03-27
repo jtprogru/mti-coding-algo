b6875606a1233afb2403fa8c17366f76ae2cf721
Michael Savin <jtprogru@gmail.com>
2023-03-27 20:24:14 +0300

    upd: Add test lecture00 with test app for SRE interview
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

---
+++ lecture00/src/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 8192
+
+static int resolve(const char *name)
+{
+  char buffer[BUF_SIZE];
+  int i;
+
+  printf("Starting read file...\n");
+  int input_fd = open(name, O_RDONLY);
+  if ( input_fd == -1 )
+  {
+    return 1;
+  }
+  ssize_t bytes_read = read (input_fd, &buffer, BUF_SIZE);
+  printf("%.*s", int(bytes_read), buffer);
+  close (input_fd);
+  printf("Successfully exited\n");
+  return 0;
+}
+
+static int do_connect()
+{
+  const char *name = "/tmp/test";
+  return resolve(name);
+}
+
+int main()
+{
+  int code = do_connect();
+  return code;
+}
