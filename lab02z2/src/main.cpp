41e23dfaf0ae8010d4f396f4776ff3385cc8ccbe
Michael Savin <jtprogru@gmail.com>
2023-04-03 00:24:14 +0300

    upd: Add full task lab02z2

---
+++ lab02z2/src/main.cpp
@@ -0,0 +1,43 @@
+/*
+ * PROGRAMM: lab02z2
+ * Проверить, является ли данное натуральное
+ * число числом Фибоначчи.
+ *
+ * Блок-схема доступна тут:
+ * https://app.code2flow.com/f2JHSsAhxUQe
+ *
+ * Copyright [2023] Michael Savin
+ */
+
+#include <iostream>
+
+int main()
+{
+    int number, a = 0, b = 1, c = 0;
+    bool isFibonacci = false;
+    std::cout << "Введите целое число: ";
+    std::cin >> number;
+
+    while (b <= number)
+    {
+        if (b == number)
+        {
+            isFibonacci = true;
+            break;
+        }
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    if (isFibonacci)
+    {
+        std::cout << number << " - число Фибоначчи" << std::endl;
+    }
+    else
+    {
+        std::cout << number << " - не число Фибоначчи" << std::endl;
+    }
+
+    return 0;
+}
