c7c4c3f065dc574244985ff79c3fa7c5103eca6e
Michael Savin <jtprogru@gmail.com>
2023-04-02 21:16:22 +0300

    fix: Add schema to lab01z2p1

--- lab01z2p1/src/main.cpp
+++ lab01z2p1/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- *
+ * PROGRAMM: lab01z2p1
  * Напишите программу, которая запрашивала бы у пользователя:
  *    - его имя (например, "What is your name?")
  *    - возраст ("How old are you?")
@@ -11,24 +11,30 @@
  * Вместо имя, возраст, место_жительства должны быть данные,
  * введенные пользователем.
  *
+ * Блок-схема доступна тут:
+ * https://app.code2flow.com/g1c8V7vmRP4X
+ *
  * Copyright [2023] Michael Savin
-*/
+ */
 
 #include <iostream>
 
 int main()
 {
-  std::string name;
-  std::uint16_t age;
-  std::string place;
-  std::cout << "What is your name?\n" << "-> " << std::endl;
-  std::cin >> name;
-  std::cout << "How old are you?\n" << "-> " << std::endl;
-  std::cin >> age;
-  std::cout << "Where are you live?\n" << "-> " << std::endl;
-  std::cin >> place;
-  std::cout << "This is " << name << std::endl;
-  std::cout << "It is " << age << std::endl;
-  std::cout << "(S)he live in " << place << std::endl;
-  return 0;
+    std::string name;
+    std::string place;
+    std::uint16_t age;
+    std::cout << "What is your name?\n"
+              << "-> " << std::endl;
+    std::cin >> name;
+    std::cout << "How old are you?\n"
+              << "-> " << std::endl;
+    std::cin >> age;
+    std::cout << "Where are you live?\n"
+              << "-> " << std::endl;
+    std::cin >> place;
+    std::cout << "This is " << name << std::endl;
+    std::cout << "It is " << age << std::endl;
+    std::cout << "(S)he live in " << place << std::endl;
+    return 0;
 }
