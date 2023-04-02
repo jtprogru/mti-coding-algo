0c61291ba84ce446466456bfdd9fabbf8f1cac62
Michael Savin <jtprogru@gmail.com>
2023-04-02 19:47:21 +0300

    fix: Add link to schema

--- lab01z1p1/src/main.cpp
+++ lab01z1p1/src/main.cpp
@@ -4,25 +4,27 @@
  * экран произведение этих чисел, сумму и разницу.
  * Покажите так же среднее арифметическое этих введенных чисел.
  *
+ * Блок-схема доступна тут:
+ * https://app.code2flow.com/XirGozucQMtL
+ *
  * Copyright [2023] Michael Savin
-*/
+ */
 
 #include <iomanip>
 #include <iostream>
 
-int main()
-{
-    float a = 0, b = 0;
-    float ma;
-    char str[5];
+int main() {
+  float a = 0, b = 0;
+  float ma;
 
-    std::cout << "Введите первое число: ";
-    std::cin >> a;
-    std::cout << "Введите второе число: ";
-    std::cin >> b;
+  std::cout << "Введите первое число: ";
+  std::cin >> a;
+  std::cout << "Введите второе число: ";
+  std::cin >> b;
 
-    ma = (a + b) / 2;
+  ma = (a + b) / 2;
 
-    std::cout << std::fixed << std::setw(3) << std::setprecision(2) << ma << std::endl;
-    return 0;
+  std::cout << std::fixed << std::setw(3) << std::setprecision(2) << ma
+            << std::endl;
+  return 0;
 }
