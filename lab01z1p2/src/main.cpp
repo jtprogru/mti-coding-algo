f645d21f7049d3b47ac2090d3792330ca0bc37ad
Michael Savin <jtprogru@gmail.com>
2023-04-02 20:41:27 +0300

    fix: Add schema

--- lab01z1p2/src/main.cpp
+++ lab01z1p2/src/main.cpp
@@ -1,24 +1,24 @@
 /*
- *
+ * PROGRAMM: lab01z1p2
  * Создайте 5 переменных типа char, предложите пользователю ввести слово
  * из пяти букв и покажите эти символы (слово) на экран.
  *
+ * Блок-схема доступна тут:
+ * https://app.code2flow.com/cpWCYandlxFG
+ *
  * Copyright [2023] Michael Savin
-*/
+ */
 
-#include <iomanip>
 #include <iostream>
 
 int main()
 {
     char q, w, e, r, t;
 
-    std::cout << "Введите слово из пяти букв:\n";
-    // TODO: Уточнить про ввод в одну строку или по символу на каждой строке
+    std::cout << "Введите слово из пяти букв:" << std::endl;
     std::cin >> q >> w >> e >> r >> t;
 
     std::cout << q << w << e << r << t << std::endl;
 
     return 0;
 }
-
