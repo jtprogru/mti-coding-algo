62df61744b7cc3ccad74e3b616da7fb4b676f919
Michael Savin <jtprogru@gmail.com>
2023-04-16 10:49:36 +0300

    upd: Add RUN PYTHON app
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

---
+++ lecture04/src/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * Задача данной программа выполнить скрипт
+ * /path/to/your/script.py с аргументами arg1 и arg2
+ * с помощью интерпретатора python3
+ * и собрать весь вывод скрипта в переменную,
+ * после чего вывести его в stdout.
+ *
+ * Copyright [2023] Michael Savin
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+    string command = "python3 /path/to/your/script.py arg1 arg2";
+    char buffer[128];
+    string result = "";
+
+    // Выполнение команды и получение результата
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        throw runtime_error("Ошибка выполнения команды");
+    }
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        result += buffer;
+    }
+    pclose(pipe);
+
+    // Вывод результата
+    cout << result;
+
+    return 0;
+}
